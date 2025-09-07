0b591a4860c4b56839d5bf8f381ce7de53b42dde
Chanan Welt <chwelt88@gmail.com>
2025-09-07 18:58:56 +0300

    add mounting for UserMsg on root cmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,6 +4,7 @@ import { About } from "./pages/About.jsx";
 import { BookIndex } from "./pages/BookIndex.jsx";
 import { BookDetails } from "./pages/BookDetails.jsx";
 import { BookEdit } from "./pages/BookEdit.jsx";
+import { UserMsg } from "./cmps/UserMsg.jsx";
 
 const { useState } = React;
 const Router = ReactRouterDOM.HashRouter;
@@ -18,19 +19,18 @@ export function RootCmp() {
       <section className="app">
         <AppHeader />
         <main className="main-layout">
-          <main>
-            <Routes>
-              <Route path="/" element={<Navigate to="/home" />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/book" element={<BookIndex />} />
-              <Route path="/book/:bookId" element={<BookDetails />} />
-              <Route path="/book/edit" element={<BookEdit />} />
-              <Route path="/book/edit/:bookId" element={<BookEdit />} />
-              <Route path="*" element={<h1>404 - Not Found</h1>} />
-            </Routes>
-          </main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/book" element={<BookIndex />} />
+            <Route path="/book/:bookId" element={<BookDetails />} />
+            <Route path="/book/edit" element={<BookEdit />} />
+            <Route path="/book/edit/:bookId" element={<BookEdit />} />
+            <Route path="*" element={<h1>404 - Not Found</h1>} />
+          </Routes>
         </main>
+        <UserMsg />
       </section>
     </Router>
   );
